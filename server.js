602750191f05d95913c9d30eeebf9aa4479babef
Benn Salter <bennsalter@gmail.com>
2015-12-10 22:08:24 -0500

    more stuff done

--- server.js
+++ server.js
@@ -15,23 +15,39 @@ app.get('/', function(req, res){
 
 app.get('/unzombify', function(req, res){
     var query = req.query.q;
-    if(query.length > 1000) {
-        res.status(414).end("414 Error: text too long.");
+    if(typeof query == "undefined") {
+        var errorobj = {"status":414,"message":"No query parameter provided"};
+        res.status(414).json(errorobj);
+    } else if(query.length > 1000) {
+        var errorobj = {"status":414,"message":"Text too long"};
+        res.status(414).json(errorobj);
+    } else {
+        var resobj = {"result": this_translator.translateToEnglish(query).translation};
+        res.json(resobj);
     }
-    var resobj = {"result":this_translator.translateToEnglish(query).translation};
-    res.json(resobj);
 });
 
 app.get('/zombify', function(req, res){
     var query = req.query.q;
-    if(query.length > 1000) {
-        res.status(414).end("414 Error: text too long.");
+    if(typeof query == "undefined") {
+        var errorobj = {"status":414,"message":"No query parameter provided"};
+        res.status(414).json(errorobj);
+    } else if(query.length > 1000) {
+        var errorobj = {"status":414,"message":"Text too long"};
+        res.status(414).json(errorobj);
+    } else {
+        var resobj = {"result": this_translator.translateToZombie(query).translation};
+        res.json(resobj);
     }
-    var resobj = {"result":this_translator.translateToZombie(query).translation}
-    res.json(resobj);
+});
+
+app.use(function(req, res) {
+    res.status(404);
+    var errorobj = {"status":404,"message":"Invalid URL"};
+    res.json(errorobj);
 });
 
 app.listen(7000, function() {
-    console.log("Listening on port 7000");
+    console.log("Server started, listening on port 7000");
 });
 
